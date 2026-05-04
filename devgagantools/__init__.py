e1d59dc94aadb36debaff047273977c35e0bec3c
Rabeel <codeblackcapital@gmail.com>
2026-05-04 07:40:21 +0400

    Delete devgagantools directory

--- devgagantools/__init__.py
+++
@@ -1,9 +0,0 @@
-import warnings
-from exztools import *
-
-warnings.warn(
-    "devgagantools is deprecated and has been renamed to exztools. "
-    "Please update your imports as this shim will be removed in a future version.",
-    DeprecationWarning,
-    stacklevel=2
-)
--- devgagantools/spylib.py
+++
@@ -1,9 +0,0 @@
-import warnings
-from exztools.exzlib import *
-
-warnings.warn(
-    "devgagantools.spylib is deprecated and has been renamed to exztools.exzlib. "
-    "Please update your imports as this shim will be removed in a future version.",
-    DeprecationWarning,
-    stacklevel=2
-)
